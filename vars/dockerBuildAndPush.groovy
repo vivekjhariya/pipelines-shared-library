23593a8c2dff513ea57225004d9b51fff873d594
vivek jhariya <154339928+vivekjhariya@users.noreply.github.com>
2026-01-15 11:39:05 +0530

    Update dockerBuildAndPush.groovy
    fixing errors

--- vars/dockerBuildAndPush.groovy
+++ vars/dockerBuildAndPush.groovy
@@ -1,19 +1,27 @@
-def call(String credId,String dockerImageName ){
-    withCredentials([userPassword(
-        credentialsId: "${credId}",
-        usernameVariable: "${dockerUser}",
-        passwordVariable: "${dockerPass}"
-    )]){
-        sh '''
-        echo "Building image: ${dockerImageName}:build-${env.BUILD_NUMBER}"
-        docker build -t ${dockerImageName}:build-${env.BUILD_NUMBER} .
-        docker login -u ${dockerUser} -p ${dockerPass}
-        docker image tag ${dockerImageName}:build-${env.BUILD_NUMBER} ${dockerUser}/${dockerImageName}:build-${env.BUILD_NUMBER}
-        docker push ${dockerUser}/${dockerImageName}:build-${env.BUILD_NUMBER}
+def call(String credId, String dockerImageName) {
 
+    withCredentials([
+        usernamePassword(
+            credentialsId: credId,
+            usernameVariable: 'DOCKER_USER',
+            passwordVariable: 'DOCKER_PASS'
+        )
+    ]) {
 
-        '''
+        sh """
+            echo "Building image: ${dockerImageName}:build-${env.BUILD_NUMBER}"
+
+            docker build -t ${dockerImageName}:build-${env.BUILD_NUMBER} .
+
+            echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
+
+            docker tag ${dockerImageName}:build-${env.BUILD_NUMBER} \
+            \$DOCKER_USER/${dockerImageName}:build-${env.BUILD_NUMBER}
+
+            docker push \$DOCKER_USER/${dockerImageName}:build-${env.BUILD_NUMBER}
+        """
     }
-
 }
+
+
 
