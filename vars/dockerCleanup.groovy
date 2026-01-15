321cacba5684c247117ce9a3cbcdd1d9fe2c7ae6
vivek jhariya <154339928+vivekjhariya@users.noreply.github.com>
2026-01-15 12:08:31 +0530

    Update dockerCleanup.groovy
    fixing errors

--- vars/dockerCleanup.groovy
+++ vars/dockerCleanup.groovy
@@ -1,12 +1,12 @@
 def call(String imageName, int keepImages = 2) {
     sh """
         echo "Keeping latest ${keepImages} images for ${imageName} and removing older ones"
-
         docker images ${imageName} --format '{{.ID}}' | \
-        awk '!seen[\\$0]++' | \
-        tail -n +$((startFrom)) | \
+        awk '!seen[\\\$0]++' | \
+        tail -n +\$(( ${keepImages} + 1 )) | \
         xargs -r docker rmi -f
     """
 }
+
 
 
