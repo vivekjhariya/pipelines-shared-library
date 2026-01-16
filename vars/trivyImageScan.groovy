9718cd03874fe3a794ffc7603d4fb737b05d3d82
vivek jhariya <154339928+vivekjhariya@users.noreply.github.com>
2026-01-16 12:43:37 +0530

    Update trivyImageScan.groovy
    adding checks

--- vars/trivyImageScan.groovy
+++ vars/trivyImageScan.groovy
@@ -3,7 +3,13 @@ def call(String imageName){
    echo "Scanning Docker image..."
 
   sh '''
-   trivy image --format table ${imageName} > trivy-image-report.txt
+
+  set -e
+   TIMESTAMP=\$(date+ "%Y%m%d-%H%M%S")
+   REPORT_NAME="trivy-image-report-${imageName}-\${TIMESTAMP}.txt"
+   trivy image --format table ${imageName} > \$REPORT_NAME
+
+   echo "report genertaed: \$REPORT_NAME"
   '''
      echo "Docker image scanned successfully"
   
