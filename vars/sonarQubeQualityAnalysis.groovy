ac6b5b5a84b19aae24e2ea684ea4e63add2bb3d1
vivek jhariya <154339928+vivekjhariya@users.noreply.github.com>
2026-01-16 22:32:04 +0530

    Update sonarQubeQualityAnalysis.groovy
    update def

--- vars/sonarQubeQualityAnalysis.groovy
+++ vars/sonarQubeQualityAnalysis.groovy
@@ -1,15 +1,20 @@
-def call(String sonarTokenName, String sonarQubeProjectName, String sonarQubeProjectKey){
-echo "sonarQube analysis start....."
-  
-  withSonarQubeEnv(credentialsId: "${sonarTokenName}"){
-    sh """
-    
-    "$SONAR_QUBE/bin/sonar-scanner" \
-    -Dsonar .projectName=$sonarQubeProjectName \
-    -Dsonar .projectKey=$sonarQubeProjectKey \
-    -X
+def call(Map config = [:]) {
 
+  def token = config.sonarQubeTokenName ?: error("SonarQube token name is required")
+  def key = config.sonarQubeProjectKey ?: error("SonarQube project key is required")
+  def name = config.sonarQubeProjectName ?: error("SonarQube project name is required")
+  def installation = config.sonarQubeInstallationName ?: error("SonarQube installation name is required")
+  def scannerHome = config.sonarQubeScannerHome ?: tool('sonar-scanner')
+
+  echo "SonarQube analysis started"
+
+  withSonarQubeEnv(credentialsId: token, installationName: installation) {
+    sh """
+      ${scannerHome}/bin/sonar-scanner \
+        -Dsonar.projectKey=${key} \
+        -Dsonar.projectName=${name}
     """
   }
-  echo "sonarQube analysis complete"
+
+  echo " SonarQube analysis completed"
 }
