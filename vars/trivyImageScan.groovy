1786f16eb484ee0d93451880a2951b804824abd5
vivek jhariya <154339928+vivekjhariya@users.noreply.github.com>
2026-01-16 16:56:55 +0530

    Update trivyImageScan.groovy
    try to fix the error

--- vars/trivyImageScan.groovy
+++ vars/trivyImageScan.groovy
@@ -2,16 +2,16 @@ def call(){
 
    echo "Scanning Docker image..."
 
-  sh '''
+  sh """
 
   set -e
-   TIMESTAMP=\$(date+ "%Y%m%d-%H%M%S")
+   TIMESTAMP=\$(date +%Y%m%d-%H%M%S)
    SAFE_TAG=\${IMAGE_TAG//[:\\/]_}
-   REPORT_NAME="trivy-image-report-${SAFE_TAG}-\${TIMESTAMP}.txt"
+   REPORT_NAME=trivy-image-report-\${SAFE_TAG}-\${TIMESTAMP}.txt
    trivy image --format table \$IMAGE_TAG > \$REPORT_NAME
 
    echo "report genertaed: \$REPORT_NAME"
-  '''
+  """
      echo "Docker image scanned successfully"
   
 }
