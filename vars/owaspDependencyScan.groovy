2a6ded73b6fd0b86b014c79b15d0fb5925474e3e
vivek jhariya <154339928+vivekjhariya@users.noreply.github.com>
2026-01-16 23:52:49 +0530

    Update owaspDependencyScan.groovy
    update fun

--- vars/owaspDependencyScan.groovy
+++ vars/owaspDependencyScan.groovy
@@ -3,19 +3,15 @@ def call(String owaspDepsName) {
 
   echo " OWASP Dependency-Check scan started..."
 
-  dependencyCheck(
-    odcInstallation: owaspDepsName,
-    additionalArguments: '''
-      --scan .
-      --format XML
-      --out dependency-check-report
-      --failOnCVSS 7
-    '''
-  )
+dependencyCheck(
+  odcInstallation: owaspDepsName,
+  additionalArguments: '--scan . --format XML --failOnCVSS 7'
+)
 
-  dependencyCheckPublisher(
-    pattern: '**/dependency-check-report/dependency-check-report.xml'
-  )
+dependencyCheckPublisher(
+  pattern: '**/dependency-check-report.xml'
+)
+
 
   echo " OWASP Dependency-Check scan complete"
 }
